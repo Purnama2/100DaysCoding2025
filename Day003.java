da97ccd681fe2a30f00432f7a6ff7b6306f4dfac
Purnama2 <purnama25082000@gmail.com>
2025-09-10 14:03:06 +0700

    Day003.java

---
+++ Day003.java
@@ -0,0 +1,21 @@
+package Day003;
+
+public class main {
+    public static void main(String[] args) {
+        
+        // Membuat variabel
+        String nama = "Purnama";
+        int umur = 18;
+        String alamat = "Paku";
+        
+        System.out.println("Nama    : " + nama);
+      /*System.out.println("Umur    : "+ umur);
+        System.out.println("Alamat  : " + alamat);*/
+    
+      // komentar satu baris 
+      /*komentar banyak baris  */
+                
+        
+              
+    }
+}
