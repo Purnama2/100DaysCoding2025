0f03cc6dbc4b72fa545893dcab638f3c64391481
Purnama2 <purnama25082000@gmail.com>
2025-09-12 15:48:27 +0700

    Day005.java

---
+++ Day005.java
@@ -0,0 +1,20 @@
+package Day005;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 25;
+        short c = 30;
+        long d = 1000;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        
+        // int menyimpan bilangan -2,147,483,648 hingga 2,147,483,647
+        // byte menyimpan bilangan bulat dari -128 hingga 127
+        // short menyimpan bilangan bulat dari -32,768 hingga 32,767
+        // Long menyimpan bilangan bilangan bulat dari -9,223,372,035,854,775,808 hingga 9,223,372,036,854,775,808
+                
+                
