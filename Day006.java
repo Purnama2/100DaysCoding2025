e508e80592245d1dfd9f62ab53534a0193540a11
Purnama2 <purnama25082000@gmail.com>
2025-09-13 12:03:31 +0700

    Day006.java

---
+++ Day006.java
@@ -0,0 +1,17 @@
+package Day006;
+
+public class main {
+    public static void main(String[] args) {
+        float a = 21.55f;
+        double b = 22.0;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        // float mentimpan angka pecahan, cukup untuk menyimpan 6 hingga 7 digit desimal
+        // dauble menyimpan angka pecahan, cukup untuk menyimpan 15 hingha 16 digit desimal
+        
+        
+    }
+    
+}
