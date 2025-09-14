82fe359dd60c6681c10dde9187ff2e4c97b590f1
Purnama2 <purnama25082000@gmail.com>
2025-09-14 14:25:25 +0700

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+package Day007;
+
+public class main {
+    public static void main(String[] args) {
+        char inisial = 'P';
+        boolean  b = true;
+        
+        System.out.println("inisial : " + inisial);
+        System.out.println("sudah login : " + b);
+        
+        // char adalah tipe data karakter, contoh P
+        // boolean adalah tipe data yang hanya bernilai true dan false (benar dan salah)
+    }
+    
+}
