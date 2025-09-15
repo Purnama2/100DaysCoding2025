6790eeacd7c009cdd3279123821cced2b93baf00
Purnama2 <purnama25082000@gmail.com>
2025-09-15 17:44:56 +0700

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,23 @@
+package Day008;
+
+public class main {
+    public static void main(String[] args) {
+        
+        String nama = "Purnama";
+        String prodi = "Informatika";
+        String fakultas ="Teknik";
+        String nim = "D0225027";
+        String alamat = "Polewali Mandar";
+        
+        System.out.println("Nama        : "+ nama);
+        System.out.println("Prodi       : "+prodi);
+        System.out.println("Fakultas    : "+ fakultas);
+        System.out.println("NIM         : "+ nim);
+        System.out.println("Alamat      : "+ alamat);
+        
+        /*String digunakan untuk menyimpan data teks apapaun, seperti kata,kalimat, atau bahkan nomor telepon.
+        String di deklarasikan dengan menempatkan teks di antara tanda kutip ganda, misalnya "halo semua".
+        */                       
+                
+    }
+}
