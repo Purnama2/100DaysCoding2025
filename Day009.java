d29e3beb057dcbb20fe55dbfe551ed3610427f29
Purnama2 <purnama25082000@gmail.com>
2025-09-16 18:47:19 +0700

    Day009.java

---
+++ Day009.java
@@ -0,0 +1,35 @@
+package Day009;
+
+public class main {
+    public static void main(String[] args) {
+        //deklarasi variabel
+        String nama;
+        int umur;
+        
+        //inisialisasi variabel
+        nama = "Nur";
+        umur = 18;
+        
+        //menampilkan nilai awal 
+        System.out.println("Nama : "+ nama);
+        System.out.println("umur : "+ umur);
+       
+        // update variabel 
+        nama = "Siti";
+        umur = 20;
+        
+        //menampilkan nilai setelah diupdate
+        System.out.println("nama :"+ nama);
+        System.out.println("umur :"+ umur);
+        
+        // deklarasi, membuat variabel (menentukan nama dan tipe.
+        /* inisialisasi, memberi nilai awal ke variabel yang sudah 
+        dideklarasikan kadang deklarasi dan inisialisasi dilakukan bersamaan.*/
+        // update variabel, mengubah nilai variabel yang sudah ada dalam variabel dengan nilai baru.
+        
+                
+                
+                
+    }
+    
+}
