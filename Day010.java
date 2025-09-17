2e4e8a5844daebda4a797a523f4807b4ce707a02
Purnama2 <purnama25082000@gmail.com>
2025-09-17 18:49:50 +0700

    Day010.java

---
+++ Day010.java
@@ -0,0 +1,15 @@
+package Day010;
+
+public class main {
+    public static void main(String[] args) {
+        final int jumlahsiswa = 36;
+        
+        System.out.println("Jumlah Siswa : "+ jumlahsiswa);
+        
+        /*kostanta (final variabel) adalah variabel yang nilainya tidak bisa diubah 
+        lagi setelah diinisialisasi. biasanya digunakan untuk menyimpan nilai yang tetap (konstanta).*/
+
+
+    }
+    
+}
