4d6af054c16519f0a6a26156e08a74c5fcda9fab
Purnama2 <purnama25082000@gmail.com>
2025-09-22 19:46:26 +0700

    operator aritmatika pertambahan dan pengurangan

---
+++ Day.015.java
@@ -0,0 +1,25 @@
+package Day015;
+
+public class main {
+    public static void main(String[] args) {
+       //deklarasi
+        int a, b;
+        
+        //inisialisasi
+        a = 15;
+        b = 25;
+        
+        //menampilkan output
+        System.out.println("Penjumlahan : "+ (a + b));
+        System.out.println("Pengurangan : "+(a - b));
+        
+        // Operator aritmatika penjumlahan (+) dan pengurangan (-).
+        // Disini saya menggunakan tipe data int 
+        /* Operator arimatika tidak hanya menggunakan tipe data int tapi juga bisa
+        menggunakan tipe data byte, long, short, float dan double.
+        */
+                
+                
+    }
+    
+}
