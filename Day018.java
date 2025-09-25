be27a79baafbd0051458a7da119586e82214b0fd
Purnama2 <purnama25082000@gmail.com>
2025-09-25 21:40:35 +0700

    konversi otomatis dari tipe data kecil ke besar

---
+++ Day018.java
@@ -0,0 +1,26 @@
+package Day018;
+
+public class main {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = a;
+        
+        short c = 120;
+        int d = c;
+        
+        int e = 1450;
+        long f = e;
+        
+        long g = 25;
+        double h = g;
+        
+        
+                
+        System.out.println("Hasil 1:"+ b);
+        System.out.println("Hasil 2:"+ d);
+        System.out.println("Hasil 3:"+ f);
+        System.out.println("Hasil 4:"+ h);
+       
+        
+    }
+}  
