2ae645e00955de30d50e18c2f44cab31b6b9a327
Purnama2 <purnama25082000@gmail.com>
2025-09-26 19:58:16 +0700

    konversi manual/paksa (dari  Tipe Data besar ke kecil).

---
+++ Day019.java
@@ -0,0 +1,15 @@
+package Day019;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = (byte) a;
+        
+        double c = 15.0;
+        float d = (float)c;
+        
+        System.out.println("nilai 1 :"+ b);
+        System.out.println("nilai 2 :"+ d);
+    }
+    
+}
