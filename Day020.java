9ad0dc0e2b662d94a6628cb8291ebe95c4834e20
Purnama2 <purnama25082000@gmail.com>
2025-09-27 20:52:14 +0700

    Mengubah tipe data primitif menjadi string

---
+++ Day020.java
@@ -0,0 +1,20 @@
+package Day020;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        String stra  = String.valueOf(a);
+        
+        double b = 50.5;
+        String strb = Double.toString(b);
+        
+        boolean c = true;
+        String strc = String.valueOf(c);
+        
+        System.out.println("Bilangan Bulat: "+ stra);
+        System.out.println("Bilangan Desimal :"+strb);
+        System.out.println("Nilai : "+  strc);
+        
+        
+    }
+}
