ddf32df6161b774dc64ad0b2b19a996ba4019aac
Purnama2 <purnama25082000@gmail.com>
2025-09-28 18:12:53 +0700

    Latihan : program untuk menukar nilai dua variabel

---
+++ Day021.java
@@ -0,0 +1,18 @@
+package Day021;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        
+        // proses penukaran
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a ="+ a);
+        System.out.println("b ="+ b);
+    }
+    
+}
