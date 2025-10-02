b4b80b69cfe163a1f1d6daeb5329c355a81a4981
Purnama2 <purnama25082000@gmail.com>
2025-10-02 19:43:37 +0700

    operator increment dan decrement(++,--)

---
+++ Day025.java
@@ -0,0 +1,19 @@
+package Day025;
+
+public class main {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        
+        a = 2;
+        b = 3;
+        c = 4;
+        d = 5;       
+        
+        System.out.println(a++); // post increment
+        System.out.println(++b); // pre increment
+        System.out.println(c--); // post decrement
+        System.out.println(--d); // pre decrement
+        
+    }
+    
+}
