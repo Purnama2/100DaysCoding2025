356a89add6302db0f1a1914d93dd9e82140a7667
Purnama2 <purnama25082000@gmail.com>
2025-10-03 19:38:10 +0700

    operator penugasan

---
+++ Day026.java
@@ -0,0 +1,35 @@
+package Day026;
+
+public class main {
+    public static void main(String[] args) {
+        //deklarasi 
+        int a, b, c, d, e, f;
+        
+        // inisialisasi variabel
+        a = 15;
+        b = 15;
+        c = 15;
+        d = 15;
+        e = 15;
+        f = 15;
+        
+        // operator penugasan
+        a += 5;
+        b -= 5;
+        c /= 5;
+        d *= 5;
+        e %= 5;
+        f = 5;
+        
+        
+        // menampilkan output
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+    
+}
