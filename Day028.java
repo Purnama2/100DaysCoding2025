d47013d95c5120f968313b868071a4acc8ae0a9b
Purnama2 <purnama25082000@gmail.com>
2025-10-05 19:23:57 +0700

    operator penugasan

---
+++ Day028.java
@@ -0,0 +1,17 @@
+package Day028;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner p = new Scanner (System.in);
+        
+        System.out.print("masukkan angka :");
+        int a = p.nextInt();
+        System.out.print("masukkan angka :");
+        int b = p.nextInt();
+        
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
