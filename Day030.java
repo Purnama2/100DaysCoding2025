2fffe564d12103f8b171bb0949bb7464513f53f7
Purnama2 <purnama25082000@gmail.com>
2025-10-07 19:56:38 +0700

    operator perbandingan <= dan >=

---
+++ Day030.java
@@ -0,0 +1,18 @@
+package Day030;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        
+        System.out.print("");
+        int a = p.nextInt();
+        System.out.print("");
+        int b = p.nextInt();
+        
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+    
+}
