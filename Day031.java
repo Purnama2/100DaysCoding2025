49566a0f878f41a985029ad036a8d113429e49fa
Purnama2 <purnama25082000@gmail.com>
2025-10-08 20:10:55 +0700

    operator logika &&

---
+++ Day031.java
@@ -0,0 +1,15 @@
+package Day031;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner p = new Scanner (System.in);
+        
+        System.out.print("");
+        int a = p.nextInt();
+        
+        System.out.println(a > 5 && 15 < a);
+    }
+    
+}
