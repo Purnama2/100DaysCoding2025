503098b7a48e061ef1589768103f5839aa34eea6
Purnama2 <purnama25082000@gmail.com>
2025-10-10 17:11:48 +0700

    operator logika !

---
+++ Day033.java
@@ -0,0 +1,16 @@
+package Day033;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+       Scanner p = new Scanner (System.in);
+       
+        System.out.print("masukkan angka :");
+        int a = p.nextInt();
+        
+        int b =  15;
+        System.out.println("apakah nilai a lebih besar dari b? \njawabannya adalah :  "+ !(a > + b) );
+    }
+ 
+}
