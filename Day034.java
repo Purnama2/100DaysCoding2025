3ced102a0c0ca5125da9d07e54d41082870c935d
Purnama2 <purnama25082000@gmail.com>
2025-10-11 22:39:09 +0700

    Update Day034.java

--- Day034.java
+++ Day034.java
@@ -6,14 +6,18 @@ public class main {
     public static void main(String[] args) {
         Scanner p = new Scanner (System.in);
         
-        System.out.print("masukkan angka :");
+        System.out.print("nilai a:");
         int a = p.nextInt();
+        System.out.print("nilai b:");
+        int b = p.nextInt();
+        System.out.print("nilai c:");
+        int c = p.nextInt();
         
-        a -= 4; // operator penugasan
-        System.out.println(a + a * a  ); // operator aritmatika
-        System.out.println((a % 2 == 0)); // operator aritmatika dan perbandingan
-        System.out.println(a + a * a < 10 ); // operator aritmatika dan perbandingan     
-        System.out.println((a + a * a > 10) || (a % 2 == 0)); //operator aritmatika, perbandingan dan logika
+        int d = a + (b * c);
+        System.out.println("Nilai akhir a = " + d);
+        System.out.println("Hasil akhir   = " + (d %2 == 0 && d / b > c|| d <= 10));
+        
+        
          }
     
 }   
