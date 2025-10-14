bf5248b8c8c66fe7ba87611b3856b7dd8f09a650
Purnama2 <purnama25082000@gmail.com>
2025-10-14 20:57:29 +0700

    latihan menggunakan bilangan ganjil dan genap

--- Day037.java
+++ Day037.java
@@ -6,17 +6,17 @@ public class main {
     public static void main(String[] args) {
         Scanner p = new Scanner (System.in);
         
-        System.out.print("masukkan angka: ");;
+        System.out.print("masukkan angka: ");
         int a = p.nextInt();
-        System.out.print("masukkan angka: ");;
+        System.out.print("masukkan angka: ");
         int b = p.nextInt();
-        System.out.print("masukkan angka: ");;
+        System.out.print("masukkan angka: ");
         int c = p.nextInt();
         
         if (a %2 == 0) {
             System.out.println(a +" angka genap");
         }else {
-            System.out.println(a +" angka ganji;");
+            System.out.println(a +" angka ganjil");
         }
         
         if (b %2 == 0) {
