87caef4007141ab45000872434318f05f8028633
Purnama2 <purnama25082000@gmail.com>
2025-10-14 20:38:30 +0700

    katinan menentukan bilangan ganjil atau genap

---
+++ Day037.java
@@ -0,0 +1,40 @@
+package Day037;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner p = new Scanner (System.in);
+        
+        System.out.print("masukkan angka: ");;
+        int a = p.nextInt();
+        System.out.print("masukkan angka: ");;
+        int b = p.nextInt();
+        System.out.print("masukkan angka: ");;
+        int c = p.nextInt();
+        
+        if (a %2 == 0) {
+            System.out.println(a +" angka genap");
+        }else {
+            System.out.println(a +" angka ganji;");
+        }
+        
+        if (b %2 == 0) {
+            System.out.println(a +" angka genap");
+        }else {
+            System.out.println(a +" angka ganjil");
+        }
+        
+        if (c %2 == 0) {
+            System.out.println(a +" angka genap");
+        }else {
+            System.out.println(a +" angka ganji");
+        }
+        
+ 
+    }
+
+
+    }
+    
+
