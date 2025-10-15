2b8e4b3a726a318be012180a87fda1372537a8ee
Purnama2 <purnama25082000@gmail.com>
2025-10-15 21:02:12 +0700

    latihan menetukan bilangan positif, negatif dan nol

---
+++ Day038.java
@@ -0,0 +1,22 @@
+package Day038;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("masukkan angka: ");
+        int angka = input.nextInt();
+        
+        if (angka > 0 ) {
+            System.out.println("angka "+ angka + " adalah bilangan  positif");
+            System.out.println("contoh lain 1, 2, 3, 100, 1000.");
+        }else if (angka < 0){
+            System.out.println("angka "+angka +" adalah bilangan negatif");
+            System.out.println("contoh lain -1, -2, -3, -100, 1000.");
+        }else{
+            System.out.println("angka "+ angka + " adalah bilangan nol");
+            System.out.println("nol bukan bilangan positif maupun negatif");
+    }
+    }
+}
