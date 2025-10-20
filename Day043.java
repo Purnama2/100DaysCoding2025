79ce446833c1b850c5cc063856dd06da66a2bcdc
Purnama2 <purnama25082000@gmail.com>
2025-10-20 18:47:13 +0700

    latihan 3,5,3dan 5

--- Day043.java
+++ Day043.java
@@ -13,14 +13,17 @@ public class main {
         String ganjilgenap = "";
         String hasil = "";
         
-        if (angka < 10) {
+        if (angka > 1 && angka < 10) {
            angka1 = "satuan ";
-        }else if (angka <100) {
+        }else if (angka >10 && angka <100) {
             angka1 = "puluhan ";
-        }else {
-            angka1 = "ratusan ";
+        }else if (angka > 100 && angka <1000 ) {
+            angka1 = "ratusan ";   
         }
         
+        
+            
+ 
         if (angka %2 == 0) {
             ganjilgenap = "genap";
         }else {
@@ -28,15 +31,14 @@ public class main {
         }
         
         if (angka %3 == 0 && angka %5==0) {
-            hasil = "yang bisa di bagi 3 dan 5";
+            hasil = " yang bisa di bagi 3 dan 5";
         }else if (angka %3 == 0) {
-            hasil = "yang bisa di bagi 3";
+            hasil = " yang bisa di bagi 3";
         }else if (angka %5 == 0) {
-            hasil = "yang bisa di bagi 5";
+            hasil = " yang bisa di bagi 5";
         }else{
             hasil = "yang tidak bisa di bagi 3 dan 5";
         }               
         
-        System.out.println(angka + " adalah angka "+ angka1 + hasil );
-}
+        System.out.println(angka + " adalah angka "+ganjilgenap + angka1 + hasil );
 }
