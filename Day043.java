9c6f7991e20023d3010b191fcd4933aa6312685f
Purnama2 <purnama25082000@gmail.com>
2025-10-20 21:58:06 +0700

    p

--- Day043.java
+++ Day043.java
@@ -16,38 +16,34 @@ public class main {
         
         
         if (angka  >1 && angka < 10){
-           angka1 = "satuan ";
+           angka1 = "adalah angka satuan ";
         }else if (angka >10 && angka <100){
-            angka1 = "puluhan ";
+            angka1 = "adalah angka puluhan ";
         }else if (angka > 100 && angka <1000 ){
-            angka1 = "ratusan ";   
+            angka1 = "adalah angka ratusan ";   
         }else{
-            angka1 = "";
+            hasil = "tidak dapat dibagi 3 dan 5";
         }
             
-  
- 
+
         if (angka %2 == 0){
-            ganjilgenap = "genap ";
+            ganjilgenap = " genap ";
         }else{
-            ganjilgenap = "ganjil ";
+            ganjilgenap = " ganjil ";
         }
         
-        
-        
-        
-        if (angka <= 0){
+        if (angka == 0){
             hasil ="tidak dapat dibagi 3 dan 5";
+        }else if (angka <= 0){
+            hasil ="tidak boleh menampilkan angka negatif";
         }else if (angka %3 == 0 && angka %5 ==0 ){
             hasil = " yang bisa di bagi 3 dan 5";
         }else if (angka %5 == 0){
             hasil = " yang bisa di bagi 5";
         }else if (angka %3==0) {
             hasil = " yang bisa di bagi 3";
-        }else{
-            hasil = " tidak dapat di bagi 3 dan 5";
-        }      
+        }
         
-        System.out.println(angka + " adalah angka "+angka1 + ganjilgenap + hasil );
+        System.out.println(angka + angka1 + ganjilgenap + hasil );
 }
 }
