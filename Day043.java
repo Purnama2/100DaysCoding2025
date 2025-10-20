1b52938a9b572f7214cd147762dc69511c660542
Purnama2 <purnama25082000@gmail.com>
2025-10-20 18:20:02 +0700

    latihan program kelipantan 3,5,3dan5

---
+++ Day043.java
@@ -0,0 +1,42 @@
+package Day043;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+    Scanner p = new Scanner (System.in);
+    
+        System.out.print("Masukkan Angka: ");
+        int angka = p.nextInt();
+        
+        String angka1 = "";
+        String ganjilgenap = "";
+        String hasil = "";
+        
+        if (angka < 10) {
+           angka1 = "satuan ";
+        }else if (angka <100) {
+            angka1 = "puluhan ";
+        }else {
+            angka1 = "ratusan ";
+        }
+        
+        if (angka %2 == 0) {
+            ganjilgenap = "genap";
+        }else {
+            ganjilgenap = "ganjil";
+        }
+        
+        if (angka %3 == 0 && angka %5==0) {
+            hasil = "yang bisa di bagi 3 dan 5";
+        }else if (angka %3 == 0) {
+            hasil = "yang bisa di bagi 3";
+        }else if (angka %5 == 0) {
+            hasil = "yang bisa di bagi 5";
+        }else{
+            hasil = "yang tidak bisa di bagi 3 dan 5";
+        }               
+        
+        System.out.println(angka + " adalah angka "+ angka1 + hasil );
+}
+}
