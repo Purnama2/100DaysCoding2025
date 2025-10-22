edc9a4b5726cfbe4ee4a1253e5724db9a03ce668
Purnama2 <purnama25082000@gmail.com>
2025-10-22 22:32:50 +0700

    program switch(case)
    Updated input method to use boolean for program selection.

--- Day045.java
+++ Day045.java
@@ -6,21 +6,17 @@ public class main {
     public static void main(String[] args) {
         Scanner p = new Scanner (System.in);
         
-        System.out.print("masukkan pilihan prodi dari 1 - 3 : ");
-        int angka = p.nextInt();
-        
-        switch(angka){
+        System.out.print("masukkan pilihan prodi dari 1 dan 2 menggunakan true/false: ");
+        boolean angka = p.nextBoolean();
+        int angka2 = (angka)? 1 : 2;
+      
+        switch(angka2){
             case 1:
                 System.out.println("Prodi: Informatika");
             break;
             case 2:
                 System.out.println("Prodi: Sistem Informasi");
-            break;
-            case 3:
-                System.out.println("Prodi: Sipil");
-                break;
-            default:
-                System.out.println("Pilihan tidak valid atau pilihan tidak tersedia.");
+            
         }
         
     }
