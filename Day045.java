8db350357cc5b0309a2fb4530ae181aac53dc8eb
Purnama2 <purnama25082000@gmail.com>
2025-10-22 20:14:24 +0700

    program switch(case)

--- Day045.java
+++ Day045.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
         Scanner p = new Scanner (System.in);
         
-        System.out.println("masukkan pilihan prodi dari 1 - 3 : ");
+        System.out.print("masukkan pilihan prodi dari 1 - 3 : ");
         int angka = p.nextInt();
         
         switch(angka){
