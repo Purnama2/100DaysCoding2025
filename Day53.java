2b067bdc711872823c6a7e638459477d5b5fcabe
Purnama2 <purnama25082000@gmail.com>
2025-10-30 23:06:39 +0800

    Keyword break

--- Day53.java
+++ Day53.java
@@ -5,39 +5,20 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("===== MANU MAKANAN =====");
-        System.out.println("1. Nasi Goreng 15/porsi");
-        System.out.println("2. Mie Goreng 12/porsi");
-        System.out.println("3. Gado-Gado 10/porsi");
+        Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan pilihan: ");
+        System.out.print("Masukkan angka: ");
         int angka = sc.nextInt();
-        System.out.print("Jumlah Porsi: ");
-        int jumlah = sc.nextInt();
         
-        int harga = 0;
-        
-        switch (angka){
-            case 1:
-                   harga = 15000;
-                   harga *= jumlah;
-                   System.out.println("Anda memilih Nasi Goreng dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
-            break;
-            case 2:
-                   harga = 12000;
-                   harga *= jumlah;
-                   System.out.println("Anda memilih Mie Goreng dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
-            break;
-            case 3:
-                   harga = 10000;
-                   harga *= jumlah;
-                   System.out.println("Anda memilih Gado-Gado dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
-            break;
-            default: 
-                   System.out.println(" Pilihan tidak valid");
-             
+        for (int i = 1; i <= angka; i++) {
+            System.out.println("Perulangan ke-" + i);
+            if (i == 10) {
+                System.out.println("angka lebih besar dari 10 perulangan di hentikan ");
+                break;
+            }
+            
         }
     }
+
+    }
     
-  }
