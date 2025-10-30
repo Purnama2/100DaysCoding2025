65a8dbaffeee4b9c0f97bba483e8070c1da99004
Purnama2 <purnama25082000@gmail.com>
2025-10-30 18:51:44 +0800

    Keyword break
    Added a food ordering system with menu options and pricing.

---
+++ Day53.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("===== MANU MAKANAN =====");
+        System.out.println("1. Nasi Goreng 15/porsi");
+        System.out.println("2. Mie Goreng 12/porsi");
+        System.out.println("3. Gado-Gado 10/porsi");
+        
+        System.out.print("Masukkan pilihan: ");
+        int angka = sc.nextInt();
+        System.out.print("Jumlah Porsi: ");
+        int jumlah = sc.nextInt();
+        
+        int harga = 0;
+        
+        switch (angka){
+            case 1:
+                   harga = 15000;
+                   harga *= jumlah;
+                   System.out.println("Anda memilih Nasi Goreng dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
+            break;
+            case 2:
+                   harga = 12000;
+                   harga *= jumlah;
+                   System.out.println("Anda memilih Gado-Gado dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
+            break;
+            case 3:
+                   harga = 10000;
+                   harga *= jumlah;
+                   System.out.println("Anda memilih Nasi Goreng dengan porsi "+ jumlah + ". Semua total pembayaran Rp"+harga);
+            break;
+            default: 
+                   System.out.println(" Pilihan tidak valid");
+             
+        }
+    }
+    
+  }
