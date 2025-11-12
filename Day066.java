ab49438788ec31a262b8a2cc899943c10fa59dd3
Purnama2 <purnama25082000@gmail.com>
2025-11-12 20:41:33 +0800

    Latihan program vertikal

---
+++ Day066.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan batas : ");
+        int batas = sc.nextInt();
+        
+        for (int i = 0; i <= batas; i++){
+                System.out.println("*");
+        }
+    }
+    
+             }
