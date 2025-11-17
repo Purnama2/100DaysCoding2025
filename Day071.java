d67c2c91d5abd1ab4f9db79cdcebffc52ffdb639
Purnama2 <purnama25082000@gmail.com>
2025-11-17 21:14:24 +0800

    Segitiga
    Implement a program to print a triangle pattern based on user input for height.

---
+++ Day071.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Day071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {       
+            for (int j = 1; j <= i; j++) {  
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
