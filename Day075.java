1a79cb8d95d77a2db22f7195251d093dd1d7a5f1
Purnama2 <purnama25082000@gmail.com>
2025-11-21 21:13:12 +0800

    Method String Dasar (length(), toUpperCase(), toLowerCase()).
    Demonstrates string manipulation methods in Java.

---
+++ Day075.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Day075 {
+    public static void main(String[] args) {
+        String nama = "Purnama";
+
+        System.out.println("Teks Asli: " + nama);
+        System.out.println("Panjang: " + nama.length());
+        System.out.println("Huruf Besar: " + nama.toUpperCase());
+        System.out.println("Huruf Kecil: " + nama.toLowerCase());
+    }
+}
+
+          
