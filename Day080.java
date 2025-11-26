fb9cb8a52e10081d50b94541c1555f584980b416
Purnama2 <purnama25082000@gmail.com>
2025-11-26 21:18:13 +0800

    Array 1 D
    Added a Java program that initializes and displays an array.

---
+++ Day080.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Day080 {
+    public static void main(String[] args) {
+        int[] angka = new int[3];  
+
+        // isi array
+        angka[0] = 5;
+        angka[1] = 10;
+        angka[2] = 20;
+
+        // tampilkan array
+        System.out.println(angka[0]);
+        System.out.println(angka[1]);
+        System.out.println(angka[2]);
+    }
+}
+
