62ab935fa16d74cb87786bd4f1672dc11f253986
Purnama2 <purnama25082000@gmail.com>
2025-11-27 22:26:31 +0800

    Latihan membuat index java dalam array

---
+++ Dau081.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Day081 {
+     public static void main(String[] args) {
+         int []a = {12, 15, 17, 18, 20};
+         
+         System.out.println("indeks 0: "+ a [0]);
+         System.out.println("indeks 0: "+ a [2]);
+         System.out.println("indeks 0: "+ a [4]);
+     }
+}
