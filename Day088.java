45511afba4e44c2334cef4299c7bf36b1eb8f2a2
Purnama2 <purnama25082000@gmail.com>
2025-12-04 20:39:16 +0800

    Method void

---
+++ Day088.java
@@ -0,0 +1,13 @@
+package maun;
+
+public class Day088 {
+     public static void main(String[] args) {
+          
+          String []nama = {"pur", "nur", "siti","edo"};
+          
+          for(String a : nama){
+               System.out.print(a + " ");
+          }
+          
+     }
+}
