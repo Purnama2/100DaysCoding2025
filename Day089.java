43974f3a99cbbf6be5ddbbd97d1fd736e4801a10
Purnama2 <purnama25082000@gmail.com>
2025-12-05 21:54:45 +0800

    Method void

---
+++ Day089.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Day089 {
+    static void nama (String nama) {
+        System.out.println("Halo!, Perkenalkan nama saya " + nama);
+    }
+
+    public static void main(String[] args) {
+        // Memanggil method void
+        String nama = "Purnama";
+        
+        nama(" purnama ");
+    }
+}
+
