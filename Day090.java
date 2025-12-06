e2b4f0afdf17d66baecf4a8c02dd4079830831e4
Purnama2 <purnama25082000@gmail.com>
2025-12-06 20:48:03 +0800

    Method non void

---
+++ Day090.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Day90 {
+    public static int tambah(int a, int b) {
+    int hasil = a + b;
+    return hasil; // mengembalikan nilai
+}
+
+public static void main(String[] args) {
+    int total = tambah(2, 6);
+    System.out.println("Hasil penjumlahan = " + total);
+}
+
+}
