b7c613ed6981ca48da2659b78cf95dc7d7032a20
Purnama2 <purnama25082000@gmail.com>
2025-12-08 21:55:22 +0800

    Keyword return

---
+++ Day092.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Day92t {
+    public static int tambah(int a, int b) {
+    int hasil = a + b;
+    return hasil; // mengembalikan nilai hasil
+}
+    public static void main(String[] args) {
+        int total = tambah(5, 3);
+        System.out.println(total); // output: 8
+
+    }
+}
