51353c18eb8f462bd0cfb2c1e254eea81dc9e3b7
Purnama2 <purnama25082000@gmail.com>
2025-12-15 20:23:26 +0800

    Latihan bilangan prima

---
+++ Day099.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.Scanner;
+
+public class Day099 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int n = sc.nextInt();
+
+        System.out.println("Bilangan prima dari 1 sampai " + n + ":");
+
+        for (int i = 2; i <= n; i++) {
+            boolean prima = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+
+            if (prima) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
